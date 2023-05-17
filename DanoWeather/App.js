deb59cf3598e6a0f37189a23ddfcf2f4c83bdb79
Lee Dano <eksh7080@gmail.com>
2023-05-17 23:45:50 +0900

    [FEAT] 1.현재 내 위치 구하기 2.스크롤 뷰 컴포넌트 사용

--- DanoWeather/App.js
+++ DanoWeather/App.js
@@ -1,20 +1,105 @@
-import {  View } from 'react-native';
+import { useEffect, useState } from 'react';
+import * as Location from 'expo-location'
+import {  View ,StyleSheet, Text , ScrollView, Dimensions} from 'react-native';
+const { height, width: SCREEN_WIDTH } = Dimensions.get('window')
+const API_KEY = "c06c9380e301b89b4ded2081a7af6cd1"
+
 
 export default function App() {
+  const [city, setCity] = useState("Loading...")
+  const [contract, setContract] = useState(true)
+  const [days, setDays] = useState([])
+  const getLocation = async() => {
+    const {granted} =  await Location.requestForegroundPermissionsAsync()
+    if(!granted) setContract(false)
+    
+
+    const {coords: {latitude, longitude}} = await Location.getCurrentPositionAsync({accuracy: 5})
+    const location = await Location.reverseGeocodeAsync({latitude, longitude}, {useGoogleMaps: false})
+    setCity(location[0].district)
+    const res =  (await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}`))
+    console.log(res, ' 리스폰 리스폰')
+  }
+
+
+  useEffect(() => {
+    getLocation()
+  },[])
+
   return (
-    <View style={{flex: 1}}>
-      <View style={{ flex: 1,backgroundColor:'coral'}}></View>
-      <View style={{ flex: 1,backgroundColor:'red'}}></View>
-      <View style={{ flex: 1,backgroundColor:'blue'}}></View>
+    <View style={styles.container}>
+      <View style={styles.city}>
+        <Text style={styles.cityName}>{city}</Text>
+      </View>
+      <ScrollView pagingEnabled horizontal showsHorizontalScrollIndicator={false}  contentContainerStyle={styles.weather}>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+      </ScrollView>
     </View>
   );
 }
 
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#fff',
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-// });
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'coral',
+    
+  },
+  city: {
+    flex:1.2,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  cityName: {
+    fontSize: 68,
+    fontWeight:'500'
+  },
+  weather: {
+  },
+  day:{
+    width: SCREEN_WIDTH,
+    alignItems: 'center'
+  },
+  temp: {
+    fontSize: 128,
+    marginTop: 40,
+  },
+  description:{
+    fontSize: 40,
+    marginTop: -40,
+  }
+});
